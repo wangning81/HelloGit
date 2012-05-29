bdf540bae4040cd01d75db421b1eada15d760150
Felix Wang <wangning81@gmail.com>
2012-05-29 22:25:16 +0800

    + First test commit.

---
+++ HelloGit/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Git!");
+            Console.ReadKey();
+        }
+    }
+}
